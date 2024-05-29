109303a339cedb6f1ee3ab3004f1c9e132202ec5
diegomsg <diegomsg@gmail.com>
2024-05-29 16:31:43 -0300

    fill_start fun

--- fun/read_files.R
+++ fun/read_files.R
@@ -7,4 +7,9 @@
 
 # fun ---------------------------------------------------------------------
 
-
+fill_start <- function(vec, size, value = NA) {
+  # fill
+  fill_n <- size - length(vec)
+  start <- rep(value, fill_n)
+  c(start, vec)
+}
