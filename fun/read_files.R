f1c02f7fd791b866f1c9236c0fb5faa237399450
diegomsg <diegomsg@gmail.com>
2024-05-31 16:23:34 -0300

    read_score function

--- fun/read_files.R
+++ fun/read_files.R
@@ -13,3 +13,33 @@ fill_start <- function(vec, size, value = NA) {
   start <- rep(value, fill_n)
   c(start, vec)
 }
+
+read_score <- function(file) {
+  lines <- vroom::vroom_lines(file) |>
+    strsplit("\\s+")
+
+  # players
+  pl_names <- lines[[1]]
+  pl_n <- length(pl_names)
+
+  # scores rows
+  scores <- lines[2:length(lines)]
+  scores_n <- length(scores)
+
+  # fix scores
+  row_sizes <- map(scores, length) |> unlist(recursive = F)
+  low_scores_ind <- which(row_sizes <= pl_n)
+  scores[low_scores_ind] <- map(scores[low_scores_ind], ~fill_start(.x, pl_n + 1))
+
+  # dealer col and initial points
+  start_score <- c(NA, rep(99, pl_n)) |>
+    as.character()
+  names(start_score) <- c("dealer", pl_names)
+
+  # scores lines to dataframe cols
+  scores_df <- data.frame(t(sapply(scores,c))) |>
+    tidyr::unnest(1)
+  colnames(scores_df) <- c("dealer", pl_names)
+
+  dplyr::bind_rows(start_score, scores_df)
+}
