5eb6ea8ed05a0eb0fa555f786ffac05576066b4d
diegomsg <diegomsg@gmail.com>
2024-05-31 17:50:36 -0300

    file main info

--- test/testing.R
+++ test/testing.R
@@ -17,18 +17,4 @@ test_file <- files[1]
 ## content -----------------------------------------------------------------
 content <- read_score(test_file)
 
-
-## name --------------------------------------------------------------------
-filename <- basename(test_file) |>
-  sub("(?<!^|[.]|/)[.][^.]+$", "", x = _, perl = TRUE)
-
-seps_regex <- "[-|_|.]"
-pattern <- glue::glue("^(?<date>\\d{2,4}{{seps_regex}}\\d{1,}[-|_]\\d{1,})(?:{{seps_regex}})(?<page>\\d*)(?:{{seps_regex}})?(?<seq>\\d*)?",
-                         .open = "{{", .close = "}}")
-matches <- stringr::str_match(filename, pattern)
-str(matches)
-id <- list(filename = matches[1],
-           date = as.Date(matches[2]),
-           page = as.integer(matches[3]),
-           seq = as.integer(matches[4]))
-
+id <- game_id(test_file)
