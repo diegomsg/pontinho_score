e621d79ab9c02828232e50f74e29f93a172c999c
diegomsg <diegomsg@gmail.com>
2024-06-06 14:58:04 -0300

    list game_id() and read_score() checking file

---
+++ fun/read_game_info.R
@@ -0,0 +1,18 @@
+
+# dependencias ------------------------------------------------------------
+
+source("src/game_id.R")
+source("src/read_score.R")
+
+
+# fun ---------------------------------------------------------------------
+
+read_game_info <- function(file) {
+  stopifnot(
+    "must be a file" = fs::is_file(file)
+  )
+  list(
+    id = game_id(file),
+    score = read_score(file)
+  )
+}
