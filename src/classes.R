7d8e059b1002e9bfca088b99f894dd7d5f27f800
diegomsg <diegomsg@gmail.com>
2024-06-06 17:22:29 -0300

    class ponto

---
+++ src/classes.R
@@ -0,0 +1,27 @@
+# dependencias ------------------------------------------------------------
+
+require(S7)
+
+
+# classes -----------------------------------------------------------------
+
+ponto <- new_class(
+  "ponto",
+  properties = list(
+    x = class_integer,
+    win = class_logical,
+    pop = class_logical
+  ),
+  validator = function(self){
+    if(!is.na(self@x)) {
+      if(self@x < 0) {"@x não pode ser menor que 0"}
+      if(self@x > 99) {"@x não pode ser maior que 99"}
+    } else {
+      if(self@win) {"@x não pode bater sem pontuação"}
+      # se jogador estourar e abandonar jogo, self@x = NA e self@pop = T
+    }
+  }
+)
+
+
+
