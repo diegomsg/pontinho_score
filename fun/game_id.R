ad0f91d1f6f8ece66d056e26f515d8657e43d93e
diegomsg <diegomsg@gmail.com>
2024-06-06 14:50:45 -0300

    isolate game_id

---
+++ fun/game_id.R
@@ -0,0 +1,16 @@
+game_id <- function(file) {
+  stopifnot(
+    is_file = fs::is_file(file)
+  )
+  filename <- basename(test_file) |>
+    sub("(?<!^|[.]|/)[.][^.]+$", "", x = _, perl = TRUE)
+
+  seps_regex <- "[-|_|.]"
+  pattern <- glue::glue("^(?<date>\\d{2,4}{{seps_regex}}\\d{1,}[-|_]\\d{1,})(?:{{seps_regex}})(?<page>\\d*)(?:{{seps_regex}})?(?<seq>\\d*)?",
+                        .open = "{{", .close = "}}")
+  matches <- stringr::str_match(filename, pattern)
+  list(id = matches[1],
+       date = as.Date(matches[2]),
+       page = as.integer(matches[3]),
+       seq = as.integer(matches[4]))
+}
