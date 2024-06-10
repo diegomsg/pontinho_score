033d146b4189ba5b54b3c15e2ba55b3265a59ab1
diegomsg <diegomsg@gmail.com>
2024-06-10 17:33:52 -0300

    ponto class to property and mao class unify fim as nested

--- src/classes.R
+++ src/classes.R
@@ -3,37 +3,54 @@
 require(S7)
 
 
-# classes -----------------------------------------------------------------
+# properties --------------------------------------------------------------
 
-ponto <- new_class(
-  "ponto",
-  properties = list(
-    x = class_integer,
-    win = class_logical,
-    pop = class_logical
-  ),
-  validator = function(self) {
-    if(!is.na(self@x)) {
-      if(self@x < 0) {"@x não pode ser menor que 0"}
-      if(self@x > 99) {"@x não pode ser maior que 99"}
-    } else {
-      if(self@win) {"@x não pode bater sem pontuação"}
-      # se jogador estourar e abandonar jogo, self@x = NA e self@pop = T
+prop_ponto <- new_property(
+  class = class_integer,
+  validator = function(value) {
+    if(length(value) != 1) {"precisa ser unitário"}
+    if(!is.na(value)) {
+      if(value < 0) {"não pode ser menor que 0"}
+      if(value > 99) {"não pode ser maior que 99"}
     }
+    # pode ser vazio, ou entre 0 e 99
   }
 )
+
+
+# classes -----------------------------------------------------------------
 
 mao <- new_class(
   "mao",
   properties = list(
-    ini = class_integer,
-    mao = class_integer,
-    fim = ponto
+    ini = prop_ponto,
+    mao = prop_ponto,
+    fim = list(
+      informado = class_integer,
+      calculado = new_property(
+        class_integer,
+        getter = function(self) {self@}
+      )
+    )
+    fim_informado = prop_ponto,
+    consistente = new_property(
+      class = class_logical,
+      setter = function(self) {self@fim_informado == self@ini - self@mao}
+    ),
+    fim = new_property(
+      class = ponto,
+      setter = function(self) {self@ini - self@mao}
+    ),
+    win = new_property(class_logical, default = F),
+    pop = new_property(class_logical, default = F)
   ),
   validator = function(self) {
     if(!is.na(self@fim)) {
       if(self@fim@x > self@ini) {"@fim não pode ser maior que @ini"}
     }
+    if(!is.na(self@x)) {
+      if(self@win) {"@x não pode bater sem pontuação"}
+      # se jogador estourar e abandonar jogo, self@x = NA e self@pop = T
   }
 )
 
